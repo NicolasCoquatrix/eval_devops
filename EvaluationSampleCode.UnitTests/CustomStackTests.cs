9cc1471c4462691afc9878c94c44e17e4f13574c
NicolasCoquatrix <c.nicolas.perso@gmail.com>
2025-01-22 16:41:06 +0100

    test

---
+++ EvaluationSampleCode.UnitTests/CustomStackTests.cs
@@ -0,0 +1,51 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EvaluationSampleCode.UnitTests
+{
+    [TestClass]
+    public sealed class CustomStackTests
+    {
+        [TestMethod]
+        public void Count_Add1_ReturnsListCount()
+        {
+            CustomStack customStack = new CustomStack();
+
+            customStack.Push(1);
+
+            Assert.AreEqual(1, customStack.Count());
+        }
+
+        [TestMethod]
+        public void Push_Add1_AddValue()
+        {
+            CustomStack customStack = new CustomStack();
+
+            customStack.Push(1);
+
+            Assert.AreEqual(1, customStack.Count());
+        }
+
+        [TestMethod]
+        public void Pop_AddAndRemoveNumber_ReturnPopedNumber()
+        {
+            CustomStack customStack = new CustomStack();
+
+            customStack.Push(1);
+
+            var customStackCount = customStack.Count();
+
+            customStack.Push(2);
+            customStack.Pop();
+
+            Assert.AreEqual(customStackCount, customStack.Count());
+        }
+
+        [TestMethod]
+        public void Pop_EmptyList_ThrowsStackCantBeEmptyException(int count)
+        {
+            CustomStack customStack = new CustomStack();
+
+            Assert.ThrowsException<ArgumentException>(() => customStack.Pop());
+        }
+    }
+}
---
+++ EvaluationSampleCode.UnitTests/ReservationTests.cs
@@ -0,0 +1,13 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EvaluationSampleCode.UnitTests
+{
+    [TestClass]
+    public sealed class ReservationTests
+    {
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+}
